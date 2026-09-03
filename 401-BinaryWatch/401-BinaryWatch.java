3010c6726828b5bb772faa8586b8153d58acfcfb
PalakUppal <palakuppal1@gmail.com>
2026-09-03 18:41:59 +0530

    File updated at 9/3/2026, 6:42:00 PM

--- 401-BinaryWatch/401-BinaryWatch.java
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -1,4 +1,4 @@
-// Last updated: 9/3/2026, 6:13:44 PM
+// Last updated: 9/3/2026, 6:42:00 PM
 1/**
 2 * Definition for singly-linked list.
 3 * class ListNode {
@@ -11,8 +11,8 @@
 10 * }
 11 */
 12public class Solution {
-13    public boolean hasCycle(ListNode head) {
-14        if (head == null || head.next == null) return false;
+13    public ListNode detectCycle(ListNode head) {
+14        if (head == null || head.next == null) return null;
 15
 16        ListNode slow = head;
 17        ListNode fast = head;
@@ -20,10 +20,19 @@
 19            slow = slow.next;
 20            fast = fast.next.next;
 21            if (slow == fast) {
-22                return true;
+22                break;
 23            }
 24        }
 25
-26        return false;
-27    }
-28}
+26        if (fast == null || fast.next == null) {
+27            return null;
+28        }
+29
+30        slow = head;
+31        while (slow != fast) {
+32            slow = slow.next;
+33            fast = fast.next;
+34        }
+35        return slow;
+36    }
+37}
